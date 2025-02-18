d04e0037e3cfeebfd6d2518bc798769db8c38469
Adam-Yung <83058286+Adam-Yung@users.noreply.github.com>
2025-02-18 10:47:05 -0800

    Update hide_sidebar.js

--- messenger/hide_sidebar.js
+++ messenger/hide_sidebar.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Messenger Hide Sidebar
 // @namespace    http://tampermonkey.net/
+
 // @version      1.2
 // @description  Hide Sidebar from page view
 // @author       Bee
@@ -24,8 +25,8 @@ function GM_addStyle(aCss) {
 
 GM_addStyle(`
 	.toggle-button-class {
-		margin-left: 5px;
-		margin-top: 5px;
+		margin-right: 5px;
+		margin-bottom: 7.5px;
 }
 `);
 
@@ -50,8 +51,7 @@ GM_addStyle(`
 
         // Remove old paths
         paths.forEach(path => path.remove());
-        toggleButton.innerHTML = '<svg class="toggle-button-class" viewBox="0 0 18 18" overflow="visible" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#CFF09E" d="M2,3 C2.55228475,3 3,3.44771525 3,4 L3,12 C3,12.5522847 2.55228475,13 2,13 C1.44771525,13 1,12.5522847 1,12 L1,4 C1,3.44771525 1.44771525,3 2,3 Z M10.2929,4.29289 C10.6834,3.90237 11.3166,3.90237 11.7071,4.29289 L15.4142,8 L11.7071,11.7071 C11.3166,12.0976 10.6834,12.0976 10.2929,11.7071 C9.90237,11.3166 9.90237,10.6834 10.2929,10.2929 L11.5858,9 L5,9 C4.44772,9 4,8.55229 4,8 C4,7.44772 4.44772,7 5,7 L11.5858,7 L10.2929,5.70711 C9.90237,5.31658 9.90237,4.68342 10.2929,4.29289 Z"></path> </g></svg>'
-
+        toggleButton.innerHTML = '<svg class="toggle-button-class" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)" stroke="#000000" stroke-width="1.2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.70710678,12 L8.85355339,15.1464466 C9.04881554,15.3417088 9.04881554,15.6582912 8.85355339,15.8535534 C8.65829124,16.0488155 8.34170876,16.0488155 8.14644661,15.8535534 L4.14644661,11.8535534 C3.95118446,11.6582912 3.95118446,11.3417088 4.14644661,11.1464466 L8.14644661,7.14644661 C8.34170876,6.95118446 8.65829124,6.95118446 8.85355339,7.14644661 C9.04881554,7.34170876 9.04881554,7.65829124 8.85355339,7.85355339 L5.70710678,11 L16.5,11 C16.7761424,11 17,11.2238576 17,11.5 C17,11.7761424 16.7761424,12 16.5,12 L5.70710678,12 Z M19,3.5 C19,3.22385763 19.2238576,3 19.5,3 C19.7761424,3 20,3.22385763 20,3.5 L20,19.5 C20,19.7761424 19.7761424,20 19.5,20 C19.2238576,20 19,19.7761424 19,19.5 L19,3.5 Z"></path> </g></svg>'
 
         button.parentNode.insertBefore(toggleButton, button.nextSibling);
 
@@ -67,16 +67,21 @@ GM_addStyle(`
         });
     }
 
-
+    let run = false;
     function add_thread_list_toggle(thread_list) {
+        if (run) {
+            return;
+        }
+        run = true;
         if (thread_list) {
             // thread_list.style.display = 'none';
 
-            // Find the target element *below* which to insert the button
+            // Find the target element below which to insert the button
 			let inbox_button =
 				document.querySelector('[role="button"][aria-label="Expand inbox sidebar"]') ||
 				document.querySelector('[role="button"][aria-label="Collapse inbox sidebar"]');
             if (inbox_button) { // Check if it exists
+                console.log("Creating button");
                 create_new_toggle(inbox_button, thread_list);
 				inbox_button.addEventListener('click', () => {
 					console.log("Run add_thread_list_toggle again after each keypress on inbox_button");
@@ -97,25 +102,27 @@ GM_addStyle(`
     }
 
     let thread_list_check;
-    function start_thread_list_check() {
-		thread_list_check = setInterval(() => {
-			console.log("Finding thread list div");
-			let thread_list = find_thread_list();
 
-			if (thread_list) {
-				console.log("Found, adding toggle button");
-				clearInterval(thread_list_check); // Stop checking once found
-				add_thread_list_toggle(thread_list);
-			}
-			else {
-				console.log("Cannot find thread list div");
-			}
-		}, 500); // Check every 500 milliseconds (adjust as needed)
-	}
+    function start_thread_list_check() {
+        clearInterval(thread_list_check); // Clear any existing interval
+        thread_list_check = setInterval(() => {
+            console.log("Finding thread list div");
+            let thread_list = find_thread_list();
+
+            if (thread_list) {
+                console.log("Found, adding toggle button");
+                clearInterval(thread_list_check);
+                thread_list_check = null; // Important: Reset the variable
+                add_thread_list_toggle(thread_list);
+            } else {
+                console.log("Cannot find thread list div");
+            }
+        }, 500);
+    }
 
     // Start checking for the message box after the page loads.
     start_thread_list_check();
-    window.on
+    // window.on
     console.log("Init %s finished", extension_name);
 
 })();
