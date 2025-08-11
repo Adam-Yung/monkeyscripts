395ba57d35eebd18ed823be72964d6807c3ed339
Adam-Yung <83058286+Adam-Yung@users.noreply.github.com>
2025-08-10 18:13:59 -0700

    Update bee_icon.js
    updated with a cute frog

--- messenger/bee_icon.js
+++ messenger/bee_icon.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       Bee
 // @match        https://www.messenger.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=messenger.com
+// @icon         https://scontent-sjc3-1.xx.fbcdn.net/v/t1.15752-9/527666362_1304318201076971_5041916544845737421_n.png?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_ohc=9UfeTkLGf2EQ7kNvwGoa0EX&_nc_oc=AdnEIfZrPGIddQ7owvbJ4yK9m3hP8rNnl42VqB78rgrA5ChFXXB_tgXv_Jt2TNxnvZqwQ5P3aHIV7brVaSOXnRF6&_nc_zt=23&_nc_ht=scontent-sjc3-1.xx&oh=03_Q7cD3AENC8K0fp8_TO6WjCzup8ec7LjmQfakrLdtgYVK23CTfQ&oe=68C09CC4
 // @run-at       document-body
 // @grant        none
 // ==/UserScript==
